4275d4b16a8dd3f25b0d439012d2f55069d6767e
slayer96 <myhailo.mykytyn.96@gmail.com>
2016-05-29 12:45:43 +0300

    edit

--- action_setting.py
+++ action_setting.py
@@ -8,16 +8,16 @@ from invenio_access.permissions import DynamicPermission
 
 from app import app
 
-
 InvenioAdmin(app, permission_factory=lambda x: x,
              view_class_factory=lambda x: x)
-
 
 access = InvenioAccess(app)
 
 action_read = ActionNeed('read')
 access.register_action(action_read)
 action_read_permission = DynamicPermission(action_read)
+
+
 @app.route('/action_read')
 @action_read_permission.require()
 def action_read():
@@ -27,25 +27,30 @@ def action_read():
 action_upload_content = ActionNeed('add_content')
 access.register_action(action_upload_content)
 upload_content_permission = DynamicPermission(action_upload_content)
+
+
 @app.route('/action_add_content')
 @upload_content_permission.require()
-def action_add_content ():
+def action_add_content():
     pass
 
 
 action_approve_content = ActionNeed('approve_content')
 access.register_action(action_approve_content)
 action_approve_content_permission = DynamicPermission(action_approve_content)
+
+
 @app.route('/action_approve_content')
 @action_approve_content_permission.require()
 def action_approve_content():
     pass
 
 
-
 action_reject_content = ActionNeed('reject_content')
 access.register_action(action_reject_content)
 action_reject_content_permission = DynamicPermission(action_reject_content)
+
+
 @app.route('/action_reject_content')
 @action_reject_content_permission.require()
 def action_reject_content():
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, print_function
 
-from flask import Flask, g, render_template
+from flask import Flask, g
 from flask.ext.principal import ActionNeed, RoleNeed
 from flask_babelex import Babel
 from flask_cli import FlaskCLI
@@ -10,14 +10,13 @@ from flask_menu import Menu
 from invenio_accounts import InvenioAccounts
 from invenio_accounts.views import blueprint
 from invenio_db import InvenioDB
-
 from invenio_access import InvenioAccess
 from invenio_access.permissions import DynamicPermission
 from action_setting import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.secret_key = ''
+
 FlaskCLI(app)
 Babel(app)
 Mail(app)
@@ -26,42 +25,9 @@ InvenioDB(app)
 InvenioAccounts(app)
 app.register_blueprint(blueprint)
 
-
-access = InvenioAccess(app)
-
-action_read = ActionNeed('read')
-access.register_action(action_read)
-
-action_add_content = ActionNeed('add_content')
-access.register_action(action_add_content)
-
-action_approve_content = ActionNeed('approve_content')
-access.register_action(action_approve_content)
-
-
-action_reject_content = ActionNeed('reject_content')
-access.register_action(action_reject_content)
-
-
-@app.route("/")
-def index():
-    identity = g.identity
-    actions = {}
-    for action in access.actions:
-        actions[action.value] = DynamicPermission(action).allows(identity)
-
-    if current_user.is_anonymous:
-        return render_template("invenio_access/open.html",
-                               actions=actions,
-                               identity=identity)
-    else:
-        return render_template("invenio_access/limited.html",
-                               message='',
-                               actions=actions,
-                               identity=identity)
-
-
 admin_permission = DynamicPermission(RoleNeed('admin'))
+
+
 @app.route('/role_admin')
 @admin_permission.require()
 def role_admin():
@@ -70,14 +36,10 @@ def role_admin():
     for action in access.actions:
         actions[action.value] = DynamicPermission(action).allows(identity)
 
-    message = ''
-    return render_template("base.html",
-                           message=message,
-                           actions=actions,
-                           identity=identity)
-
 
 content_submission_permission = DynamicPermission(RoleNeed('content_submission'))
+
+
 @app.route('/role_content_submission')
 @content_submission_permission.require()
 def role_content_submission():
@@ -87,42 +49,26 @@ def role_content_submission():
     actions[action_reject_content.value] = action_reject_content_permission.allows(identity)
 
 
-    message = ''
-    return render_template("invenio_access/limited.html",
-                           message=message,
-                           actions=actions,
-                           identity=identity)
-
-
-
 content_contributors_permission = DynamicPermission(RoleNeed('content_contributors'))
+
+
 @app.route('/role_content_contributors')
 @content_contributors_permission.require()
 def role_content_contributors():
     identity = g.identity
     actions = {}
-
     actions[action_upload_content.value] = upload_content_permission.allows(identity)
-
-    message = ''
-    return render_template("base.html",
-                           message=message,
-                           actions=actions,
-                           identity=identity)
 
 
 visitors_permission = DynamicPermission(RoleNeed('visitors'))
+
+
 @app.route('/role_visitors')
 @visitors_permission.require()
 def role_visitors():
     identity = g.identity
     actions = {}
     actions[action_read.value] = action_read_permission.allows(identity)
-    message = ''
-    return render_template("base.html",
-                           message=message,
-                           actions=actions,
-                           identity=identity)
 
 
 if __name__ == '__main__':
