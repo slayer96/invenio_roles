1e6742ac957a7430ea9fa3366fbe1b8ff7857cba
slayer96 <myhailo.mykytyn.96@gmail.com>
2016-05-29 12:53:52 +0300

    edit

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from invenio_accounts.views import blueprint
 from invenio_db import InvenioDB
 from invenio_access import InvenioAccess
 from invenio_access.permissions import DynamicPermission
-from action_setting import *
+from invenio_admin import InvenioAdmin
+# from action_setting import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -25,7 +26,58 @@ InvenioDB(app)
 InvenioAccounts(app)
 app.register_blueprint(blueprint)
 
+InvenioAdmin(app, permission_factory=lambda x: x,
+             view_class_factory=lambda x: x)
+
+access = InvenioAccess(app)
+
+action_read = ActionNeed('read')
+access.register_action(action_read)
+action_read_permission = DynamicPermission(action_read)
+
+
+@app.route('/action_read')
+@action_read_permission.require()
+def action_read():
+    pass
+
+
+action_upload_content = ActionNeed('add_content')
+access.register_action(action_upload_content)
+upload_content_permission = DynamicPermission(action_upload_content)
+
+
+@app.route('/action_add_content')
+@upload_content_permission.require()
+def action_add_content():
+    pass
+
+
+action_approve_content = ActionNeed('approve_content')
+access.register_action(action_approve_content)
+action_approve_content_permission = DynamicPermission(action_approve_content)
+
+
+@app.route('/action_approve_content')
+@action_approve_content_permission.require()
+def action_approve_content():
+    pass
+
+
+action_reject_content = ActionNeed('reject_content')
+access.register_action(action_reject_content)
+action_reject_content_permission = DynamicPermission(action_reject_content)
+
+
+@app.route('/action_reject_content')
+@action_reject_content_permission.require()
+def action_reject_content():
+    pass
+
+
 admin_permission = DynamicPermission(RoleNeed('admin'))
+
+
 
 
 @app.route('/role_admin')
@@ -71,5 +123,3 @@ def role_visitors():
     actions[action_read.value] = action_read_permission.allows(identity)
 
 
-if __name__ == '__main__':
-    app.run()
